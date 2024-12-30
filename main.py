525d7510017d01129e8eaa46ea18191b4699be5c
zjx <zjx@163.com>
2024-12-30 11:02:00 +0800

    提交一下，download按钮，优化了

--- main.py
+++ main.py
@@ -7,38 +7,18 @@ from tkinter import *
 
 class mainWindow(tk.Tk):
     def __init__(self):
-        # initialize the widow
         super().__init__()
         super().title('Manhuagui Downloader')
         super().geometry('400x160')
         baseY = 30
-        # initialize the labels
         tk.Label(self, text='Url:', font=('Arial', 16,)).place(x=10, y=baseY)
         tk.Label(self, text='To:', font=('Arial', 16,)).place(x=10, y=baseY + 40)
-        # initialize the labels
         self.var_address = tk.StringVar()
         self.var_url = tk.StringVar()
         self.var_address.set('manga/')
-        # 风云第一部
-        # self.var_url.set('https://www.manhuagui.com/comic/943/')
-        #风云第三部
-        # self.var_url.set('https://www.manhuagui.com/comic/838/')
-        # 天子传奇6
-        # self.var_url.set('https://www.manhuagui.com/comic/3370/')
-        # 天子传奇8
-        # self.var_url.set('https://www.manhuagui.com/comic/3274/')
-        # 中华英雄前传
-        # self.var_url.set('https://www.manhuagui.com/comic/27892/')
-        # 天子传奇3
-        # self.var_url.set('https://www.manhuagui.com/comic/45263/')
-        # 剑风传奇烙印战士更新至378
-        # self.var_url.set('https://www.manhuagui.com/comic/1769/')
-
         self.var_url.set('https://www.manhuagui.com/comic/3047/')
-        tk.Entry(self, textvariable=self.var_url, font=('Arial', 14), width=28).place(x=60, y=baseY)  # url field
-        tk.Entry(self, textvariable=self.var_address, font=('Arial', 14), width=28).place(x=60,
-                                                                                          y=baseY + 40)  # address field
-        # initialize the button
+        tk.Entry(self, textvariable=self.var_url, font=('Arial', 14), width=28).place(x=60, y=baseY)
+        tk.Entry(self, textvariable=self.var_address, font=('Arial', 14), width=28).place(x=60, y=baseY + 40)
         tk.Button(self, text='Download', font=('Arial', 12), command=self.download).place(x=290, y=baseY + 80)
         self.mainloop()
 
@@ -53,17 +33,25 @@ class mainWindow(tk.Tk):
 
 
 class downloadPanel(Toplevel):
-    # begin to download
     def __init__(self, s):
         super().__init__()
         super().title('Manhuagui Downloader')
-        super().geometry('900x160')
-        super().geometry('900x' + str(40 * (s.length // 6) + 260))
-        # initialize labels
+        super().geometry('900x600')
+
+        self.canvas = Canvas(self)
+        self.frame = Frame(self.canvas)
+        self.scrollbar = Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.scrollbar.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
+
+        self.frame.bind("<Configure>", lambda event, canvas=self.canvas: self.onFrameConfigure(canvas))
+
         self.place_label(s)
-        # iniitialie select buttons
         self.place_buttons(s)
-        # initialize check all functions
+
         var = IntVar()
 
         def checkAll():
@@ -73,48 +61,36 @@ class downloadPanel(Toplevel):
                 elif i.cget("state") == 'normal':
                     i.deselect()
 
-        # check all button
-        # tk.Checkbutton(self, text='Select All', font=('Arial', 18), variable=var,
-        #                command=checkAll).place(x=0, y=self.baseY + 80)
-        tk.Checkbutton(self, text='Select All', font=('Arial', 18), variable=var,
-                       command=checkAll).place(x=0, y=0)
-        # dowmload buttons
-        tk.Button(self, text='Download', font=('Arial', 16),
-                  command=lambda: self.downloadChapters(s)).place(x=450, y=self.baseY + 80)
+        tk.Checkbutton(self.frame, text='Select All', font=('Arial', 18), variable=var, command=checkAll).pack()
+        tk.Button(self.frame, text='Download', font=('Arial', 16), command=lambda: self.downloadChapters(s)).pack()
 
-        # tk中的列表可以滚动
-        self.scrollbar = Scrollbar(self)
-        self.scrollbar.pack(side=RIGHT, fill=Y)
-        self.listbox = Listbox(self, yscrollcommand=self.scrollbar.set)
-        for i in s.existedChapters():
-            self.listbox.insert(END, i)
-
-        # tk中的列表可以滚动
-        self.mainloop()
+    def onFrameConfigure(self, canvas):
+        canvas.configure(scrollregion=canvas.bbox("all"))
 
     def place_label(self, s):
-        tk.Label(self, text=s.title, font=('Arial', 33,)).place(x=10, y=10)
-        tk.Label(self, text="作者: " + s.author, font=('Arial', 12,)).place(x=10, y=70)
-        tk.Label(self, text="年代: " + s.year, font=('Arial', 12,)).place(x=160, y=70)
-        tk.Label(self, text="地区: " + s.region, font=('Arial', 12,)).place(x=280, y=70)
-        tk.Label(self, text="类型: " + s.plot, font=('Arial', 12,)).place(x=400, y=70)
+        tk.Label(self.frame, text=s.title, font=('Arial', 33,)).pack()
+        tk.Label(self.frame, text="作者: " + s.author, font=('Arial', 12,)).pack()
+        tk.Label(self.frame, text="年代: " + s.year, font=('Arial', 12,)).pack()
+        tk.Label(self.frame, text="地区: " + s.region, font=('Arial', 12,)).pack()
+        tk.Label(self.frame, text="类型: " + s.plot, font=('Arial', 12,)).pack()
         self.baseY = 120
 
     def place_buttons(self, s):
         self.buttons = []
         for i in range(len(s.chapters)):
             s.chapters[i][2] = IntVar()
-            cha = tk.Checkbutton(self, text=s.chapters[i][0], font=('Arial', 14), variable=s.chapters[i][2])
-            cha.place(x=(i % 6) * 150, y=self.baseY + (i // 6) * 40)
+            cha = tk.Checkbutton(self.frame, text=s.chapters[i][0], font=('Arial', 14), variable=s.chapters[i][2])
+            cha.pack()
             if s.chapters[i][0] in s.existedChapters():
                 cha.select()
                 cha.config(state='disabled')
             self.buttons.append(cha)
-        self.baseY += (s.length // 6) * 40
 
     def downloadChapters(self, s):
         for i in range(s.length):
             if self.buttons[i].cget("state") == 'normal' and s.chapters[i][2].get():
                 s.downloadChapter(s.chapters[i][1])
+
+
 if __name__ == '__main__':
     mainWindow()
